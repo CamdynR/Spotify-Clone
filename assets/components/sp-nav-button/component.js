37d2e4a70c694935793d770d5f94b2c28a982489
CamdynR <rasque.camdyn@gmail.com>
2021-12-05 21:25:03 -0800

    Created the first component

--- assets/components/sp-nav-button/component.js
+++ assets/components/sp-nav-button/component.js
@@ -1,1 +1,19 @@
 // sp-nav-button/component.js
+
+import styles from '../../css/components.css' assert { type: 'css' };
+
+class SpNavButton extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.innerHTML = `
+      <a draggable="false" class="root sp-nav-button">
+        <slot name="icon"></slot>
+        <slot name="text"></slot>
+      </a>
+    `;
+    this.shadowRoot.adoptedStyleSheets = [styles];
+  }
+}
+
+customElements.define('sp-nav-button', SpNavButton);
